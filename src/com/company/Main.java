5c4f8e3fbd0a8900787e36b92ee9487a94413441
Muhammet Cagri Karakaya <mcagrikarakaya@gmail.com>
2020-03-22 13:22:01 +0100

    smal changing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,5 +25,9 @@ public class Main {
         HealthyBurger healthyBurger2 = new HealthyBurger("kase",3);
         healthyBurger1.addHamburgerAddition2("egg",1);
         System.out.println("Total Price is " + healthyBurger2.itemizeHamburger());
+
+        HealthyBurger healthyBurger3 = new HealthyBurger("kase",3);
+        healthyBurger1.addHamburgerAddition3("egg",1);
+        System.out.println("Total Price is " + healthyBurger3.itemizeHamburger());
     }
 }
